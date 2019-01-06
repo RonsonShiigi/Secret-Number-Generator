a3d8927de146aa1b9b6c3c177b155e1b55cee1c4
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-01-05 16:28:25 -1000

    pau

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
+    var j = Math.random() * 1000;
+  var secretNumber = function(){
+      return j
+      
+      
+
+  }
+  return secretNumber
+
+
+  
   
 };
